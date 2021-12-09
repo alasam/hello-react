47201738e310ad76b74cc1e5cf3fe25312ed2509
Anthony <alasam@live.com>
2021-12-08 19:25:52 -0500

    add input field

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ const Button = (props) => {
   const text = props.text;
   const reset = props.reset;
   return (
-    <button onClick = { () => console.log(props.reset)}>
+    <button onClick = { () => console.log({reset})}>
     <span>{text}</span>
     </button>
   )
 };
 
 const Application = () => {
-
+  const [name, setName] = useState("Anthony");
   // your code here
 
   const reset = () => {
@@ -23,8 +23,9 @@ const Application = () => {
 
   return (
     <main>
+      <input placeholder="Type your name" />
       <Button text="reset" reset="reset"/>
-      <h1>Hello React</h1>
+      <h1>Hello {name}</h1>
     </main>
   );
 };
